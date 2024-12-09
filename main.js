1cae73f6a3b77b5b79547c4170223a34fe15abae
Bandclay-j <talisontalison57@gmail.com>
2024-12-09 11:04:39 -0300

    Portfolio

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const carousels = document.querySelectorAll('.carousel');
+
+    carousels.forEach(carousel => {
+        const images = carousel.querySelectorAll('.carousel-image');
+        const radios = carousel.querySelectorAll('input[type="radio"]');
+        let currentIndex = 0;
+
+        function updateCarousel(index) {
+            radios[index].checked = true;
+            const offset = -index * 100;
+            const imagesContainer = carousel.querySelector('.carousel-images');
+            imagesContainer.style.transform = `translateX(${offset}%)`;
+        }
+
+        function autoSlide() {
+            currentIndex = (currentIndex + 1) % images.length;
+            updateCarousel(currentIndex);
+        }
+
+        setInterval(autoSlide, 3000); // Muda a imagem a cada 3 segundos
+
+        radios.forEach((radio, index) => {
+            radio.addEventListener('change', () => {
+                currentIndex = index;
+                updateCarousel(index);
+            });
+        });
+    });
+});
+
 function ativarModo() {
     let body = document.body;
     let botao = document.getElementById('botao');
